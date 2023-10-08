2161f9e65534e29545aca55045d7adf66beec5ad
Takamasa Oshikiri <mail@oshikiri.org>
2023-10-08 19:08:47 +0900

    Refactor area/d3.js

--- examples/area/d3.js
+++ examples/area/d3.js
@@ -1,38 +1,35 @@
+const svgWidth = 372;
+const svgHeight = 242;
+
 const margin = { top: 10, right: 10, bottom: 40, left: 60 };
-const width = 372 - margin.left - margin.right;
-const height = 242 - margin.top - margin.bottom;
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
 
 const parseYearMonth = d3.timeParse("%Y-%m");
 
-const chart = d3
+const svg = d3
   .select("#graph-d3js")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom);
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-const svg = chart
+const chart = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-chart
-  .append("text")
-  .attr("class", "axis-label")
-  .attr("font-size", 10)
-  .attr("font-weight", "bold")
-  .attr("text-anchor", "end")
+appendAxisLabel(svg)
   .attr("x", margin.left + width / 2)
   .attr("y", margin.top + height + margin.bottom / 2)
   .text("date");
 
-chart
-  .append("text")
-  .attr("font-size", 10)
-  .attr("font-weight", "bold")
-  .attr("text-anchor", "end")
+appendAxisLabel(svg)
   .attr("transform", "rotate(-90)")
   .attr("x", -height / 2)
   .attr("y", margin.left / 2)
   .text("count");
+
+const xScale = d3.scaleTime().range([0, width]);
+const yScale = d3.scaleLinear().range([height, 0]);
 
 d3.json("../../data/unemployment-across-industries.json").then((data) => {
   const aggregated = d3
@@ -43,30 +40,47 @@ d3.json("../../data/unemployment-across-industries.json").then((data) => {
     )
     .map(([k, v]) => ({ date: k, count: v }));
 
-  const xScale = d3
-    .scaleTime()
-    .domain(d3.extent(aggregated.map((d) => d.date)))
-    .range([0, width]);
-  svg
+  xScale.domain(d3.extent(aggregated.map((d) => d.date)));
+  yScale.domain([0, 16000]);
+
+  appendYAxis(chart);
+  appendXAxis(chart);
+  appendArea(chart, aggregated);
+});
+
+function appendAxisLabel(svg) {
+  return svg
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "end");
+}
+
+function appendYAxis(chart) {
+  return chart
     .append("g")
     .attr("class", "grid")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(xScale).ticks(5).tickSize(-height));
+}
 
-  const yScale = d3.scaleLinear().domain([0, 16000]).range([height, 0]);
-  svg
+function appendXAxis(chart) {
+  return chart
     .append("g")
     .attr("class", "grid")
     .call(d3.axisLeft(yScale).ticks(5).tickSize(-width));
+}
 
+function appendArea(chart, aggregated) {
   const valueline = d3
     .area()
     .x((d) => xScale(d.date))
     .y0(height)
     .y1((d) => yScale(d.count));
-  svg
+  return chart
     .append("path")
     .datum(aggregated)
     .attr("class", "line")
     .attr("d", valueline);
-});
+}
