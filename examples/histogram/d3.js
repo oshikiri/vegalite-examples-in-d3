1210110eaa134656f1e178be6e8381bf11fcb062
Takamasa Oshikiri <mail@oshikiri.org>
2023-10-08 20:23:44 +0900

    Refactor histogram/

--- examples/histogram/d3.js
+++ examples/histogram/d3.js
@@ -1,90 +1,87 @@
-// https://observablehq.com/@d3/histogram/2?intent=fork
-d3.json("../../data/movies.json").then((data) => {
-  const width = 258;
-  const height = 247;
-  const marginTop = 20;
-  const marginRight = 0;
-  const marginBottom = 30;
-  const marginLeft = 50;
+const svgWidth = 258;
+const svgHeight = 247;
+const marginTop = 20;
+const marginRight = 0;
+const marginBottom = 30;
+const marginLeft = 50;
 
+const width = svgWidth - marginLeft - marginRight;
+const height = svgHeight - marginTop - marginBottom;
+
+const svg = d3
+  .select("#graph-d3js")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+const chart = svg
+  .append("g")
+  .attr("transform", `translate(${marginLeft}, ${marginTop})`);
+
+svg
+  .append("text")
+  .attr("font-size", 10)
+  .attr("font-weight", "bold")
+  .attr("text-anchor", "middle")
+  .attr("x", marginLeft + width / 2)
+  .attr("y", marginTop + height + 0.9 * marginBottom)
+  .text("IMDB Rating (binned)");
+
+svg
+  .append("text")
+  .attr("font-size", 10)
+  .attr("font-weight", "bold")
+  .attr("text-anchor", "middle")
+  .attr("transform", `rotate(-90)`)
+  .attr("x", -marginTop - height / 2)
+  .attr("y", 0.3 * marginLeft)
+  .text("Count of Records");
+
+const xScale = d3.scaleLinear().range([0, width]);
+const yScale = d3.scaleLinear().range([height, 0]);
+
+d3.json("../../data/movies.json").then((data) => {
   const bins = d3
     .bin()
     .thresholds(10)
     .value((d) => d["IMDB Rating"])(data);
 
-  const x = d3
-    .scaleLinear()
-    .domain([bins[0].x0, bins[bins.length - 1].x1])
-    .range([marginLeft, width - marginRight]);
+  xScale.domain([0, 10]);
+  yScale.domain([0, 1000]);
 
-  const y = d3
-    .scaleLinear()
-    .domain([0, 1000])
-    .range([height - marginBottom, marginTop]);
-
-  const svg = d3
-    .select("#graph-d3js")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [0, 0, width, height]);
-
-  svg
-    .append("g")
-    .selectAll("line")
-    .data(y.ticks(5))
-    .join("line")
-    .attr("x1", marginLeft)
-    .attr("x2", width - marginRight)
-    .attr("y1", (d) => y(d))
-    .attr("y2", (d) => y(d))
-    .attr("stroke", "grey")
-    .attr("stroke-opacity", "0.3");
-
-  svg
-    .append("g")
-    .attr("fill", "steelblue")
-    .selectAll()
-    .data(bins)
-    .join("rect")
-    .attr("x", (d) => x(d.x0) + 1)
-    .attr("y", (d) => y(d.length))
-    .attr("width", (d) => x(d.x1) - x(d.x0) - 1)
-    .attr("height", (d) => y(0) - y(d.length));
-
-  svg
-    .append("g")
-    .attr("transform", `translate(0, ${height - marginBottom})`)
-    .call(
-      d3
-        .axisBottom(x)
-        .ticks(width / 80)
-        .tickSizeOuter(0)
-    )
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", marginLeft + (width - marginLeft) / 2)
-        .attr("y", marginBottom - 4)
-        .attr("fill", "currentColor")
-        .attr("font-weight", "bold")
-        .text("IMDB Rating (binned)")
-    );
-  svg
-    .append("g")
-    .attr("transform", `translate(${marginLeft}, 0)`)
-    .call(d3.axisLeft(y).ticks(height / 40))
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", -(height - marginTop - marginBottom) / 2)
-        .attr("y", (-2 * marginLeft) / 3)
-        .attr("font-size", 10)
-        .attr("fill", "currentColor")
-        .attr("font-weight", "bold")
-        .attr("transform", `rotate(-90)`)
-        .text("Count of Records")
-    );
+  appendXAxis(chart);
+  appendYAxis(chart);
+  appendBars(chart, bins);
 });
 
-// TODO: Refactor x,y,transform
+function appendXAxis(chart) {
+  return chart
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(
+      d3
+        .axisBottom(xScale)
+        .tickFormat(d3.format(".1f"))
+        .tickValues(d3.range(1, 10, 2))
+    );
+}
+function appendYAxis(chart) {
+  chart
+    .append("g")
+    .call(d3.axisLeft(yScale).ticks(5).tickSize(-width).tickFormat(""))
+    .style("stroke-opacity", 0.3);
+  return chart.append("g").call(d3.axisLeft(yScale).ticks(5));
+}
+
+function appendBars(chart, bins) {
+  return chart
+    .append("g")
+    .selectAll("rect")
+    .data(bins)
+    .join("rect")
+    .attr("fill", "steelblue")
+    .attr("x", (d) => xScale(d.x0))
+    .attr("y", (d) => yScale(d.length))
+    .attr("width", (d) => xScale(d.x1) - xScale(d.x0))
+    .attr("height", (d) => height - yScale(d.length));
+}
