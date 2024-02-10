3ab3b8e02981d9be6a588e7677cab3cf1084c5e8
Takamasa Oshikiri <mail@oshikiri.org>
2024-02-10 20:37:52 +0900

    Split line_overlay code into many functions

--- examples/line_overlay/d3.js
+++ examples/line_overlay/d3.js
@@ -13,25 +13,8 @@ const svg = d3
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom);
 
-svg
-  .append("text")
-  .attr("class", "axis-label")
-  .attr("font-size", 10)
-  .attr("font-weight", "bold")
-  .attr("text-anchor", "middle")
-  .attr("x", margin.left + width / 2)
-  .attr("y", margin.top + height + 0.75 * margin.bottom)
-  .text("date (year)");
-
-svg
-  .append("text")
-  .attr("font-size", 10)
-  .attr("font-weight", "bold")
-  .attr("text-anchor", "middle")
-  .attr("transform", "rotate(-90)")
-  .attr("x", -margin.top - height / 2)
-  .attr("y", margin.left / 3)
-  .text("Mean of price");
+appendLabelX(svg, width, height, margin);
+appendLabelY(svg, height, margin);
 
 const xScale = d3.scaleLinear().range([0, width]);
 const yScale = d3.scaleLinear().range([height, 0]);
@@ -39,7 +22,34 @@ const symbolScale = d3.scaleOrdinal().range(colors).domain(symbols);
 
 const parseDate = d3.timeParse("%b %d %Y");
 
+const chart = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+xScale.domain([2000, 2010]);
+yScale.domain([0, 550]);
+
+appendAxisX(chart, xScale, height);
+appendAxisY(chart, yScale, width);
+
+const line = d3
+  .line()
+  .x(([y, _p]) => xScale(y))
+  .y(([_y, p]) => yScale(p));
+
 d3.csv("../../data/stocks.csv").then((data) => {
+  const table = preprocess(data);
+  console.log(table);
+
+  table.forEach(([symbol, data]) => {
+    console.log(data);
+    appendLine(chart, data, xScale, yScale, symbolScale(symbol), line);
+  });
+
+  appendLegend(svg, margin, width, height);
+});
+
+function preprocess(data) {
   data.forEach((d) => {
     d.date = parseDate(d.date);
     d.year = d.date.getFullYear();
@@ -53,65 +63,87 @@ d3.csv("../../data/stocks.csv").then((data) => {
     (d) => d.symbol,
     (d) => d.year
   );
-  console.log(table);
 
-  xScale.domain([2000, 2010]);
-  yScale.domain([0, 550]);
+  return table;
+}
 
-  const chart = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+function appendLabelX(parent, width, height, margin) {
+  return parent
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .attr("x", margin.left + width / 2)
+    .attr("y", margin.top + height + 0.75 * margin.bottom)
+    .text("date (year)");
+}
 
-  chart
+function appendLabelY(parent, height, margin) {
+  return parent
+    .append("text")
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -margin.top - height / 2)
+    .attr("y", margin.left / 3)
+    .text("Mean of price");
+}
+
+function appendAxisX(parent, xScale, height) {
+  const axisRoot = parent.append("g");
+  axisRoot
     .append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(xScale).ticks(5).tickFormat(d3.format("d")));
-
-  chart
+  axisRoot
     .append("g")
     .attr("class", "grid")
     .attr("transform", `translate(0, ${height})`)
     .attr("opacity", "0.7")
     .attr("stroke-width", 0.2)
     .call(d3.axisBottom(xScale).ticks(5).tickSize(-height).tickFormat(""));
+  return axisRoot;
+}
 
-  chart.append("g").call(d3.axisLeft(yScale).ticks(5));
-
-  chart
+function appendAxisY(parent, yScale, width) {
+  const axisRoot = parent.append("g");
+  axisRoot.append("g").call(d3.axisLeft(yScale).ticks(5));
+  axisRoot
     .append("g")
     .attr("class", "grid")
     .attr("transform", `translate(${width}, 0)`)
     .attr("opacity", "0.7")
     .attr("stroke-width", 0.2)
     .call(d3.axisLeft(yScale).ticks(5).tickSize(width).tickFormat(""));
+  return axisRoot;
+}
 
-  const line = d3
-    .line()
-    .x(([y, p]) => xScale(y))
-    .y(([y, p]) => yScale(p));
+function appendLine(parent, data, xScale, yScale, color, line) {
+  const lineElement = parent.append("g");
 
-  table.forEach(([symbol, data]) => {
-    console.log(data);
-    const perSymbol = chart.append("g");
+  lineElement
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("fill", color)
+    .attr("cx", ([year, _price]) => xScale(year))
+    .attr("cy", ([_year, price]) => yScale(price))
+    .attr("r", (_d) => 3);
+  lineElement
+    .append("path")
+    .attr("class", "line")
+    .attr("d", line(data))
+    .style("fill", "none")
+    .style("stroke", color)
+    .style("stroke-width", 2);
 
-    perSymbol
-      .selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("fill", symbolScale(symbol))
-      .attr("cx", ([year, price]) => xScale(year))
-      .attr("cy", ([year, price]) => yScale(price))
-      .attr("r", (d) => 3);
-    perSymbol
-      .append("path")
-      .attr("class", "line")
-      .attr("d", line(data))
-      .style("fill", "none")
-      .style("stroke", symbolScale(symbol))
-      .style("stroke-width", 2);
-  });
+  return lineElement;
+}
 
+function appendLegend(svg, margin, width, height) {
   const squareSize = 10;
   const paddingSquares = 3;
   const legendPaddingLeft = 15;
@@ -148,4 +180,6 @@ d3.csv("../../data/stocks.csv").then((data) => {
     .attr("x", legendPaddingLeft)
     .attr("y", 10)
     .text("symbol");
-});
+
+  return legend;
+}
