39820dc060018bc7c2c5f6ee7ccc85e57a3d0383
Takamasa Oshikiri <mail@oshikiri.org>
2024-08-24 22:46:42 +0900

    Throw Plotter class away

--- examples/stacked_bar_weather/d3.js
+++ examples/stacked_bar_weather/d3.js
@@ -1,193 +1,174 @@
 async function main() {
-  const plotter = new Plotter({
+  const size = {
     root: { width: 380, height: 242 },
     margin: { top: 10, right: 90, bottom: 40, left: 45 },
-  });
-  plotter.keys = ["sun", "snow", "rain", "fog", "drizzle"];
+    chart: {},
+  };
+  size.chart.width = size.root.width - size.margin.left - size.margin.right;
+  size.chart.height = size.root.height - size.margin.top - size.margin.bottom;
 
-  const data = await plotter.fetchData("../../data/seattle-weather.csv");
+  const scale = {
+    x: d3.scaleBand(),
+    y: d3.scaleLinear(),
+    color: d3.scaleOrdinal(),
+  };
 
-  const months = d3.range(12);
-  plotter.scale.x.domain(months).range([0, plotter.chartWidth]).padding(0.1);
-  plotter.scale.y.domain([0, 120]).range([plotter.chartHeight, 0]);
-  const colors = ["#e7ba52", "#9467bd", "#1f77b4", "#c7c7c7", "#aec7e8"];
-  plotter.scale.color.range(colors);
+  const data = await fetchData("../../data/seattle-weather.csv");
 
-  const root = plotter.initializeRoot();
-  plotter.appendLabels(root);
-  plotter.appendLegends(root);
-  const bars = plotter.appendBars(root, data);
-  plotter.appendGrids(bars);
+  scale.x.domain(d3.range(12)).range([0, size.chart.width]).padding(0.1);
+  scale.y.domain([0, 120]).range([size.chart.height, 0]);
+  scale.color.range(["#e7ba52", "#9467bd", "#1f77b4", "#c7c7c7", "#aec7e8"]);
+
+  const root = initializeRoot(size);
+  appendLabels(root, size);
+  appendLegends(root, size, scale);
+  const bars = appendBars(root, data, size, scale);
+  appendGrids(bars, size, scale);
 }
 
-class Plotter {
-  constructor(size) {
-    const margin = size.margin;
-    const rootSize = size.root;
+async function fetchData(url) {
+  const data = await d3.csv(url);
+  const parseMonth = d3.timeParse("%Y-%m-%d");
+  const counts = d3.rollups(
+    data,
+    (g) => g.length,
+    (d) => parseMonth(d.date).getMonth(),
+    (d) => d.weather
+  );
+  return counts;
+}
 
-    this.margin = margin;
-    this.rootSize = rootSize;
-    this.chartWidth = rootSize.width - margin.left - margin.right;
-    this.chartHeight = rootSize.height - margin.top - margin.bottom;
+function initializeRoot(size) {
+  const root = d3
+    .select("#graph-d3js")
+    .append("svg")
+    .attr("width", size.root.width)
+    .attr("height", size.root.height);
+  return root;
+}
 
-    this.scale = {
-      x: d3.scaleBand(),
-      y: d3.scaleLinear(),
-      color: d3.scaleOrdinal(),
-    };
-  }
+function appendLabels(root, size) {
+  const margin = size.margin;
+  const chartWidth = size.chart.width;
+  const chartHeight = size.chart.height;
 
-  async fetchData(url) {
-    const data = await d3.csv(url);
-    const parseMonth = d3.timeParse("%Y-%m-%d");
-    const counts = d3.rollups(
-      data,
-      (g) => g.length,
-      (d) => parseMonth(d.date).getMonth(),
-      (d) => d.weather
-    );
-    return counts;
-  }
+  const xlabel = root
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .attr("x", margin.left + chartWidth / 2)
+    .attr("y", margin.top + chartHeight + 0.75 * margin.bottom)
+    .text("Month of the year");
 
-  initializeRoot() {
-    const root = d3
-      .select("#graph-d3js")
-      .append("svg")
-      .attr("width", this.rootSize.width)
-      .attr("height", this.rootSize.height);
-    return root;
-  }
+  const ylabel = root
+    .append("text")
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -chartHeight / 2)
+    .attr("y", margin.left / 3)
+    .text("Count of Records");
 
-  appendLabels(root) {
-    const margin = this.margin;
-    const chartWidth = this.chartWidth;
-    const chartHeight = this.chartHeight;
+  return { x: xlabel, y: ylabel };
+}
 
-    const xlabel = root
-      .append("text")
-      .attr("class", "axis-label")
-      .attr("font-size", 10)
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "middle")
-      .attr("x", margin.left + chartWidth / 2)
-      .attr("y", margin.top + chartHeight + 0.75 * margin.bottom)
-      .text("Month of the year");
+function appendBars(root, counts, size, scale) {
+  const margin = size.margin;
+  const bars = root
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    const ylabel = root
-      .append("text")
-      .attr("font-size", 10)
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "middle")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -chartHeight / 2)
-      .attr("y", margin.left / 3)
-      .text("Count of Records");
+  counts.forEach(([month, d]) => {
+    appendBarGroup(bars, month, d, size, scale);
+  });
 
-    return { x: xlabel, y: ylabel };
-  }
+  return bars;
+}
 
-  appendBars(root, counts) {
-    const margin = this.margin;
-    const bars = root
-      .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+function appendBarGroup(bars, month, d, size, scale) {
+  d = this.appendCumsum(d.sort().reverse());
 
-    counts.forEach(([month, d]) => {
-      this.appendBarGroup(bars, month, d);
-    });
+  const barGroup = bars.append("g").attr("class", "bar-column");
+  barGroup
+    .selectAll("rect")
+    .data(d)
+    .enter()
+    .append("rect")
+    .attr("fill", (d) => scale.color(d.weather))
+    .attr("x", scale.x(month))
+    .attr("y", (d) => scale.y(d.cumsum))
+    .attr("height", (d) => size.chart.height - scale.y(d.count))
+    .attr("width", scale.x.bandwidth());
+  return barGroup;
+}
 
-    return bars;
-  }
+function appendCumsum(d) {
+  let cumsum = 0;
+  return d.map(([weather, count]) => {
+    cumsum += count;
+    return { weather, count, cumsum };
+  });
+}
 
-  appendBarGroup(bars, month, d) {
-    d = this.appendCumsum(d.sort().reverse());
+function appendGrids(bars, size, scale) {
+  const months = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(" ");
+  bars
+    .append("g")
+    .attr("class", "grid")
+    .attr("transform", `translate(0, ${size.chart.height})`)
+    .call(d3.axisBottom(scale.x).tickFormat((i) => months[i]));
+  bars.append("g").attr("class", "grid").call(d3.axisLeft(scale.y).ticks(6));
+}
 
-    const scale = this.scale;
-    const barGroup = bars.append("g").attr("class", "bar-column");
-    barGroup
-      .selectAll("rect")
-      .data(d)
-      .enter()
-      .append("rect")
-      .attr("fill", (d) => scale.color(d.weather))
-      .attr("x", scale.x(month))
-      .attr("y", (d) => scale.y(d.cumsum))
-      .attr("height", (d) => this.chartHeight - scale.y(d.count))
-      .attr("width", scale.x.bandwidth());
-    return barGroup;
-  }
+function appendLegends(root, size, scale) {
+  const margin = size.margin;
+  const chartWidth = size.chart.width;
 
-  appendCumsum(d) {
-    let cumsum = 0;
-    return d.map(([weather, count]) => {
-      cumsum += count;
-      return { weather, count, cumsum };
-    });
-  }
+  const keys = ["sun", "snow", "rain", "fog", "drizzle"];
+  const squareSize = 10;
+  const paddingSquares = 3;
+  const legendPaddingLeft = 15;
+  const legend = root
+    .append("g")
+    .attr("transform", `translate(${margin.left + chartWidth}, 0)`);
+  legend
+    .selectAll("legend-square")
+    .data(keys)
+    .enter()
+    .append("rect")
+    .attr("x", margin.right / 5)
+    .attr("y", (_d, i) => legendPaddingLeft + i * (squareSize + paddingSquares))
+    .attr("width", squareSize)
+    .attr("height", squareSize)
+    .style("fill", (d) => scale.color(d));
+  legend
+    .selectAll("legend-labels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", 20 + squareSize * 1.2)
+    .attr(
+      "y",
+      (_d, i) => legendPaddingLeft + i * (squareSize + paddingSquares) + 3
+    )
+    .text((d) => d)
+    .attr("text-anchor", "left")
+    .attr("font-size", squareSize)
+    .style("alignment-baseline", "central");
 
-  appendGrids(bars) {
-    const chartHeight = this.chartHeight;
-    const xScale = this.scale.x;
-    const yScale = this.scale.y;
+  legend
+    .append("text")
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "left")
+    .attr("x", legendPaddingLeft)
+    .attr("y", 10)
+    .text("Weather type");
 
-    const months = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(" ");
-    bars
-      .append("g")
-      .attr("class", "grid")
-      .attr("transform", `translate(0, ${chartHeight})`)
-      .call(d3.axisBottom(xScale).tickFormat((i) => months[i]));
-    bars.append("g").attr("class", "grid").call(d3.axisLeft(yScale).ticks(6));
-  }
-
-  appendLegends(root) {
-    const margin = this.margin;
-    const chartWidth = this.chartWidth;
-
-    const squareSize = 10;
-    const paddingSquares = 3;
-    const legendPaddingLeft = 15;
-    const legend = root
-      .append("g")
-      .attr("transform", `translate(${margin.left + chartWidth}, 0)`);
-    legend
-      .selectAll("legend-square")
-      .data(this.keys)
-      .enter()
-      .append("rect")
-      .attr("x", margin.right / 5)
-      .attr(
-        "y",
-        (d, i) => legendPaddingLeft + i * (squareSize + paddingSquares)
-      )
-      .attr("width", squareSize)
-      .attr("height", squareSize)
-      .style("fill", (d, i) => this.scale.color.range()[i]);
-    legend
-      .selectAll("legend-labels")
-      .data(this.keys)
-      .enter()
-      .append("text")
-      .attr("x", 20 + squareSize * 1.2)
-      .attr(
-        "y",
-        (d, i) => legendPaddingLeft + i * (squareSize + paddingSquares) + 3
-      )
-      .text((d, i) => this.keys[i])
-      .attr("text-anchor", "left")
-      .attr("font-size", squareSize)
-      .style("alignment-baseline", "central");
-
-    legend
-      .append("text")
-      .attr("font-size", 10)
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "left")
-      .attr("x", legendPaddingLeft)
-      .attr("y", 10)
-      .text("Weather type");
-
-    return legend;
-  }
+  return legend;
 }
 
 main();
